0c875a0715f1b8edf5bc1d72be57e6012ea841ee
Anastasiia-Eliz <aselizarova@yandex.ru>
2022-09-14 19:03:04 +0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from project.config import config
 from project.server import create_app
-
+from flask_migrate import Migrate
+from create_tables import db
 app = create_app(config)
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
 	app.run(port=25000)
